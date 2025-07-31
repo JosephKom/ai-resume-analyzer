f731fd9dca8ea325d03fa13888da010d2980c761
“JosephKom” <jokmr17@gmail.com>
2025-07-31 12:56:46 -0700

    Added authentication page and image load error
    -created user authentication check
    -created redirect for user sign in

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -5,7 +5,8 @@ import ScoreCircle from './ScoreCircle'
 const ResumeCard = ({resume : { id, companyName, jobTitle, feedback, imagePath}} : {resume: Resume}) => {
   return (
     <Link to={`/resume/${id}`} className='resume-card animate-in fade-in duration-1000'>
-        <div className='resume-card-header'>
+        <div className='resume-card-header flex flex-col gap-4'>
+        <div className='flex justify-between items-start'>
             <div className='flex flex-col gap-2'>
             <h2 className='!text-black font-bold break-words'>
                 {companyName}
@@ -17,15 +18,17 @@ const ResumeCard = ({resume : { id, companyName, jobTitle, feedback, imagePath}}
         <div className='flex-shrink-0'>
             <ScoreCircle score={feedback.overallScore}/>
         </div>
+        </div>
         <div className='gradient-border animate-in fade-in duration-1000'></div>
-            <div className='w-full h-full'>
+            <div className='w-full h-[350px] max-sm:h-[200px] overflow-hidden rounded-md'>
                 <img
                 src={imagePath}
                 alt='resume'
-                className='w-full h-[350px] max-sm:h-[200px] object-cover object-top'
+                className='w-full h-full object-cover object-top'
                 />
             </div>
         </div>
+        
     </Link>
   )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import { resumes } from "./constants";
 import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,15 +15,23 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        if(!auth.isAuthenticated)
+            navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
     <section className="main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
 
-    </section>
+    
     {resumes.length > 0 &&(
     <div className="resumes-section">
       {resumes.map((resume) =>(
@@ -30,6 +41,7 @@ export default function Home() {
     </div>
 
     )}
+    </section>
    
 
     
